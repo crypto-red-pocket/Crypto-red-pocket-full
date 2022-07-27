8964e8cb727af9ea59fffe638602404ebf5ddc31
JDPolanco <juandabiada@gmail.com>
2022-07-27 08:53:34 +0200

    Getters included and change payment function
    Getters for creators and participants. Call native payment functions substitutes transfer

--- src/contracts/RedEnvelope.sol
+++ src/contracts/RedEnvelope.sol
@@ -2,42 +2,94 @@
 pragma solidity ^0.8.0;
 
 contract RedEnvelope {
-  uint envelopeCounter;
   struct Envelopes{
     uint tokenAmount;
     uint participantsLimit;
     uint participantsCounter;
+    string message;
+    uint creationTime;
   }
-  mapping(uint => Envelopes) envelope;
+  mapping(bytes => Envelopes) envelope;
+  mapping(address => bytes []) receiverToEnvelope;
+  mapping(address => bytes []) creatorToEnvelope;
 	constructor () 
 	{
 	}
 
 	/********************************************************
-	*                                                      *
-	*                     MAIN FUNCTIONS                   *
-	*                                                      *
+	*                                                       *
+	*                     MAIN FUNCTIONS                    *
+	*                                                       *
 	********************************************************/
 
-	function createEnvelope(uint _tokenAmount, uint _participantsLimit) external payable{
-		envelopeCounter++;
+	function createEnvelope(uint _tokenAmount, uint _participantsLimit, string memory _message) external payable{
     require(msg.value == _tokenAmount, "Insufficient funds");
-    envelope[envelopeCounter].tokenAmount = _tokenAmount;
-    envelope[envelopeCounter].participantsLimit = _participantsLimit;
+    bytes memory _envelopeId =  abi.encode(msg.sender, block.timestamp);
+    envelope[_envelopeId].tokenAmount = _tokenAmount;
+    envelope[_envelopeId].participantsLimit = _participantsLimit;
+    envelope[_envelopeId].message = _message;
+    creatorToEnvelope[msg.sender].push(_envelopeId);
 	}
 
-  function claim(address _claimer, uint _envelopeId) external {
+  function claim(bytes memory _envelopeId) external {
     Envelopes storage _envelope = envelope[_envelopeId];
     require(_envelope.participantsLimit >  _envelope.participantsCounter, "max participants exceeded");
     require(_envelope.tokenAmount > 0, "tokens already distributed");
     _envelope.participantsCounter++;
     uint _amountToDeliver;
     if(_envelope.participantsLimit != _envelope.participantsCounter) {
-      _amountToDeliver = (uint(keccak256(abi.encodePacked(_envelope.participantsCounter,block.timestamp))) % _envelope.tokenAmount) + 1;
+      _amountToDeliver = _getAmountToDeliver(_envelope.participantsCounter, _envelope.tokenAmount);
     } else {
       _amountToDeliver = _envelope.tokenAmount;
     }
     _envelope.tokenAmount -= _amountToDeliver;
-    payable(_claimer).transfer(_amountToDeliver);
+    (bool sent, ) = payable(msg.sender).call{value: _amountToDeliver}("");
+    require(sent, "Failed to send Ether");
+  }
+
+	/********************************************************
+  *                                                       *
+  *                INTERNAL FUNCTIONS                     *
+  *                                                       *
+  ********************************************************/
+
+  function _getAmountToDeliver(uint _participantsCounter, uint _tokenAmount) internal view returns (uint _amountToDeliver) {
+    _amountToDeliver = (uint(keccak256(abi.encodePacked(_participantsCounter,block.timestamp))) % _tokenAmount) + 1;
+  }
+
+	/********************************************************
+  *                                                       *
+  *                     GET FUNCTIONS                     *
+  *                                                       *
+  ********************************************************/
+
+  function getCreatorEnvelopes(address _creatorAddress) external view returns (Envelopes[] memory) {
+    bytes[] memory _creatorToEnvelope = creatorToEnvelope[_creatorAddress];
+    Envelopes[] memory _envelopes = new Envelopes[](_creatorToEnvelope.length);
+    for(uint i = 0; i < _creatorToEnvelope.length; i++) {
+      _envelopes[i] = Envelopes(
+        _envelopes[i].tokenAmount,
+        _envelopes[i].participantsLimit,
+        _envelopes[i].participantsCounter,
+        _envelopes[i].message,
+        _envelopes[i].creationTime
+      );
+    }
+    return _envelopes;
+  }
+
+  function getReceiverEnvelopes(address _ReceiverAddress) external view returns (Envelopes[] memory) {
+    bytes[] memory _receiverToEnvelope = receiverToEnvelope[_ReceiverAddress];
+    Envelopes[] memory _envelopes = new Envelopes[](_receiverToEnvelope.length);
+    for(uint i = 0; i < _receiverToEnvelope.length; i++) {
+      _envelopes[i] = Envelopes(
+        _envelopes[i].tokenAmount,
+        _envelopes[i].participantsLimit,
+        _envelopes[i].participantsCounter,
+        _envelopes[i].message,
+        _envelopes[i].creationTime
+      );
+    }
+    return _envelopes;
   }
 }
