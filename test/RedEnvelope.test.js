d7c47306146632047965622240b22395f440b5ac
JDPolanco <juandabiada@gmail.com>
2022-07-27 23:40:32 +0200

    feat: test scripts updated

--- test/RedEnvelope.test.js
+++ test/RedEnvelope.test.js
@@ -2,7 +2,7 @@ const RedEnvelope = artifacts.require('RedEnvelope');
 const { expectRevert, expectEvent, time } = require('@openzeppelin/test-helpers')
 
 contract('Collection Pizzerias', (accounts) => {
-  let redEnvelope, creator, receipt
+  let redEnvelope, creator, receipt, envelopeId
   before(async() => {
     redEnvelope = await RedEnvelope.new()
     creator = accounts[0]
@@ -10,12 +10,19 @@ contract('Collection Pizzerias', (accounts) => {
 
   describe("Create Envelope", ()=> {
     it("Envelope Created succesfully", async()=> {
-      await redEnvelope.createEnvelope(5,"Hello there!", { value: 500})
-      receipt = await redEnvelope.getCreatorEnvelopes(creator)
+      receipt = await redEnvelope.createEnvelope(5,"Hello there!", { value: 500})
+      await redEnvelope.getCreatorEnvelopes(creator)
       console.log(receipt.toString())
     })
     it("EnvelopeCreated Event emited", async() => {
-      await expectEvent(receipt,'')
+      envelopeId = await redEnvelope.creatorToEnvelope(creator,0)
+      await expectEvent(receipt,'EnvelopCreated', {
+        envelopeId: envelopeId.toString()
+      }
+      )
+    })
+    it("Creator getter works", async()=> {
+      
     })
   })
 
